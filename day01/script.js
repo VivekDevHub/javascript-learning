3e595fb072b3d87ccf311f5e8cab5f97af716fa9
Vivek-MacbookPro <cashify@CASHIFYs-MacBook-Pro.local>
2026-02-08 16:19:11 +0530

    day01 variable,console,operations

---
+++ day01/script.js
@@ -0,0 +1,59 @@
+// -------------------- Console Methods -----------------------------
+// console.log('This is normal text');
+// console.warn('This is Warning!')
+// console.error('This is Error') 
+
+// semi colon is optional not compulsory
+// console.log(hello); //no existence of hello, so it give error Uncaught ReferenceError: hello is not defined
+
+var a; //declaration
+    a = 20; // initialization
+    a=50;  //  re-initialization
+console.log(a,'is value of a');
+
+// alert('This is alert message here. !')
+
+
+// var user = confirm('are you follow Vivek Dev Hub');
+// console.log(user);
+
+// var num = prompt('Enter a number');
+// console.log(num);
+
+
+// alert - for message 
+// confirm - yes or no ? 
+// prompt - write anything in input 
+
+// -------------------- Airthmethic Operation -----------------------------
+var a = 20;
+var b = 10;
+
+console.log(a+b, 'addition');
+console.log(a-b, 'subtraction');
+console.log(a*b, 'multiplication');
+console.log(a/b, 'division');
+console.log(a%b, 'modulus');
+
+
+
+var a ='hello';
+var b = 20;
+console.log(a*b); // 2 diffreent data type not possible multiply, division, any other operatioin except addition
+console.log(a+b); // plus operator work both addition or concatenation
+
+var a = 'vivek';
+var b = 'kushwah';
+console.log(a-b); // show NaN
+
+
+
+// undefined concept
+var w ;
+var b = 20;
+console.log(w); //bydefault assign undefined 
+console.log(b);
+
+
+
+
