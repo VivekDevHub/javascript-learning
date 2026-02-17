d5db820638b87f497d3bf5f5427bccbd1d138ed0
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-02-18 00:43:16 +0530

    dom assignment 10program complete

--- day21/script.js
+++ day21/script.js
@@ -1,7 +1,7 @@
 // Hello to Welcome and Welcome to Hello
-var textChange = document.getElementById("btn");
+var changeButon = document.getElementById("btn");
 
-textChange.addEventListener("click", function () {
+changeButon.addEventListener("click", function () {
   let para = document.getElementById("text");
 
   if (para.innerText === "Hello") {
@@ -36,10 +36,10 @@ var hidebtn = document.getElementById("hidebtn");
 var hidebox = document.querySelector(".hidebox");
 
 hidebtn.addEventListener("click", function () {
-  if (hidebox.style.display !== "none") {
-    hidebox.style.display = "none";
-  } else {
+  if (hidebox.style.display == "none") {
     hidebox.style.display = "block";
+  } else {
+    hidebox.style.display = "none";
   }
 });
 
@@ -60,4 +60,88 @@ minusbtn.addEventListener("click", function () {
   if (value > 0) {
     number.innerHTML = value - 1;
   }
+});
+
+//// ***************  DOM TASK 6  ***************
+//toggle theme
+var themebtn = document.querySelector("#themebtn");
+themebtn.addEventListener("click", function () {
+  if (document.body.style.backgroundColor === "white") {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+  } else {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+  }
+});
+
+//// ***************  DOM TASK 7  ***************
+// show input text
+
+var input = document.querySelector("#input");
+var output = document.querySelector("#output");
+
+input.addEventListener("input", function () {
+  const value = input.value;
+
+  output.textContent = value === "" ? "Start Typing.." : value;
+});
+// input event → runs every time you type
+// output.textContent = input.value → whatever is typed appears in paragraph
+
+//// ***************  DOM TASK 8 ***************
+// change color on mouse hover
+
+var boxx = document.querySelector(".boxx");
+
+boxx.addEventListener("mouseenter", function () {
+  boxx.style.backgroundColor = "white";
+});
+
+boxx.addEventListener("mouseleave", function () {
+  boxx.style.backgroundColor = "violet";
+});
+
+//// ***************  DOM TASK 9 ***************
+
+const btn = document.querySelector("#onceBtn");
+const msg = document.querySelector("#msg");
+
+btn.addEventListener(
+  "click",
+  function () {
+    msg.textContent = "Button Clicked";
+  },
+  { once: true },
+);
+
+//// ***************  DOM TASK 10 ***************
+
+var stopBtn = document.querySelector("#stopBtn");
+var readyBtn = document.querySelector("#readyBtn");
+var goBtn = document.querySelector("#goBtn");
+
+var red = document.querySelector("#red");
+var yellow = document.querySelector("#yellow");
+var green = document.querySelector("#green");
+
+function resetLights() {
+  red.style.backgroundColor = "gray";
+  yellow.style.backgroundColor = "gray";
+  green.style.backgroundColor = "gray";
+}
+
+stopBtn.addEventListener("click", function () {
+  resetLights();
+  red.style.backgroundColor = "red";
+});
+
+readyBtn.addEventListener("click", function () {
+  resetLights();
+  yellow.style.backgroundColor = "yellow";
+});
+
+goBtn.addEventListener("click", function () {
+  resetLights();
+  green.style.backgroundColor = "green";
 });
