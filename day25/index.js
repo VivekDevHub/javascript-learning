d6e2b97ba64b5b955eb8eda34fe5306fc08cd56a
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-02-23 23:31:50 +0530

    day25 event detial snake,piano project

---
+++ day25/index.js
@@ -0,0 +1,27 @@
+//************************. BHEEM ROTATE GAME. *************************************
+// var bheemimg = document.querySelector("#imgbheem");
+// var body = document.body;
+// var moveX = 0;
+// var moveY = 0;
+
+// body.addEventListener("keydown", function (dets) {
+//   if (dets.code === "ArrowRight") {
+//     if (moveX < 80) moveX++;
+//   }
+
+//   if (dets.code === "ArrowLeft") {
+//     if (moveX > 0) moveX--;
+//   }
+
+//   if (dets.code === "ArrowUp") {
+//     if (moveY > 0) moveY--;
+//   }
+
+//   if (dets.code === "ArrowDown") {
+//     if (moveY < 50) moveY++;
+//   }
+//   bheemimg.style.left = `${moveX}%`;
+//   bheemimg.style.top = `${moveY}%`;
+// });
+
+//************************. VIRTUAL PIANO  *************************************
---
+++ day25/resources.md
@@ -0,0 +1,4 @@
+EVENT DETAILS
+- koi bhi event perform kra toh uski details chahiye jaise mene wheel event perform kara toh meko uski detail chahiye ki scroll hua toh kaha hua, kaha gaya, kaha ruka, 
+kitni der scroll kra everything...
+
